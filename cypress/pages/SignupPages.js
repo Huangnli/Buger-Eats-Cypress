af1ba8bf4628fc1a0a2d9463e03dc3cb1fe62f9c
Huangnli <50835276+Huangnli@users.noreply.github.com>
2022-05-11 13:42:38 -0400

    Ajustes na funcao alertMessageShouldBe

--- cypress/pages/SignupPages.js
+++ cypress/pages/SignupPages.js
@@ -42,7 +42,8 @@ class SingupPages {
     }
 
     alertMessageShouldBe(expectedMessage) {
-        cy.get('.alert-error').should('have.text', expectedMessage)
+        // cy.get('.alert-error').should('have.text', expectedMessage)
+        cy.contains('.alert-error', expectedMessage).should('be.visible')
     }
 }
 
